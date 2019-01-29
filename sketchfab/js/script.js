7cdcc434645592c0399ec8c5ddeb13c5ae41f47e
plix <palle.torsson@gmail.com>
2019-01-29 14:09:16 +0100

    adding sketchfab example

---
+++ sketchfab/js/script.js
@@ -0,0 +1,81 @@
+﻿
+ $('document').ready( function(){
+
+    var iframe = document.getElementById( 'api-frame' );
+    // create json of uids and titles, etc
+    var uids = ['0cd9b9f9bfa1433c8cfa99d3ab13c11a','8c160beba7d2447dbbba6b0f48595147',  '4e3c2f9dc7b84996bf0f7b32ae55a772']
+    var currentNum = 0; 
+ 
+    var client = new Sketchfab( iframe );
+    var theTime = 2000; 
+    var api = { };
+
+    client.init( 'd809ec01e6234a91a8bae8d6b7b0e438', {
+        success: function onSuccess( api ){
+            api = api;
+            api.start();
+            api.addEventListener( 'viewerready', function() {
+                api = api;
+                setTimeout(function(){
+                    loadModel( client, uids[currentNum] );
+                    currentNum++;
+                    if (currentNum > 2) {
+                        currentNum = 0; 
+                    }
+                    theTime = 10000; 
+                }, theTime);
+            });
+        },
+        error: function onError() {
+            console.log( 'Viewer error' );
+        }
+    });
+
+    function loadModel( client, urlid ) {
+
+        client.init( urlid, {
+            success: success,
+            error: error,
+            /* This is where you can add additional options like Autospin */
+            //autospin: 0.5
+        });
+    }
+
+    var success = function( api ) {
+        api.start();
+        api.lookat(
+            [0, 0, 0], // eye position
+            [0, 0, 0], // target to lookat
+            0.1 // duration of the animation in seconds
+        );
+       api.lookat(
+            [0, 0, 0], // eye position
+            [10, 0, 0], // target to lookat
+            10.1 // duration of the animation in seconds
+        );
+
+api.createAnnotation(
+    [-0.16144215216577174, -3.867954799445659, 0.8214845269137563],
+    [0.1229991557663267, -3.5779795878788656, -0.5151466147866559],
+    camera.position,
+    camera.target,
+    'mytitle2',
+    'mytext2'
+);
+        api.getCameraLookAt(function(err, camera) {
+            console.log(camera.position);
+            console.log(camera.target);
+        });
+
+
+    };
+
+    var error = function () {
+        console.error( 'Sketchfab API Error!' );
+    };
+
+
+
+
+
+})
---
+++ sketchfab/js/script2.js
@@ -0,0 +1,116 @@
+﻿    // get pos here https://labs.sketchfab.com/experiments/screenshots/#model/74f6b21bb0fc4a31bf53cc947b94eb15
+    // http://jsoneditoronline.org/
+    var pos2 = [{
+            "position": [3, -112, 46],
+            "target": [-1, 4, 33]
+        },
+        {
+            "position": [-1, -1, 68],
+            "target": [-1, 4, 33]
+        },
+        {
+            "position": [0, 1, 4],
+            "target": [-0, -0, -4]
+        },
+        {
+            "position": [0, 0, -4],
+            "target": [0, 0, -4]
+        },
+        {
+            "position": [0, 0, 0],
+            "target": [0, 0, 0]
+        },
+        {
+            "position": [0.09, -0.01, 1.36],
+            "target": [0.08, -0.01, 0.97]
+        },
+        {
+            "position": [0, 0, 0],
+            "target": [0, 0, 0]
+        },
+        {
+            "position": [0.08, 0.03, 0.83],
+            "target": [0.08, 0.03, 0.83]
+        },
+        // model : '38710d36d8254af69b0d8ab99e1cc1ec'
+        {
+            "position": [0.7965377472993016, -3.6551350132719236, 131.8535622634526],
+            "target": [-0.16222331224440895, -2.544987200553334, 38.47862781230249]
+        },
+        {
+            "position": [-2.5054568559317376, .307368407995564, 29.326049679061672],
+            "target": [-2.5054651573296964, .3073634412127, 29.325950148072227]
+        }
+    ];
+    var index = 0;
+    // Model
+    //$("#api-frame").hide();
+    var model = '56cce93abd8a485cbdc5bfff37a59f11',
+
+        // Two buttons
+        startButton = document.getElementById('start'),
+        leftButton = document.getElementById('left'),
+        rightButton = document.getElementById('right'),
+
+        // API version
+        version = '1.4.0',
+
+        // The iframe
+        iframe = document.getElementById('api-frame'),
+
+        // The API client object
+        client = new Sketchfab(version, iframe),
+
+        // The API object
+        api,
+
+        // Defining error and success outside initialization, for easy reading
+        error = function() {
+            console.error('Sketchfab API Error!');
+        },
+
+        // If initilization succeeds, start the model immediately
+        success = function(callback) {
+            api = callback;
+            //$("#api-frame").fadeIn(5000);
+            api.start();
+            api.addEventListener(
+                'click',
+                function(info) {
+                    // console.log(info.position2D[0], info.position2D[1])
+                    if (info.position2D[0] < 100 && info.position2D[1] > 1000) {
+                        api.setCameraLookAt(pos2[index].position, pos2[index].target, 10.0);
+                        index++;
+                        console.log(index);
+                    }
+                    if (info.position2D[0] > 100 && info.position2D[1] > 1000) {
+                        api.setCameraLookAt(pos2[index - 1].position, pos2[index - 1].target, 5.1);
+                        index--;
+                        console.log(index);
+                    }
+
+                }
+            );
+        };
+
+    // A function to load a model
+    function loadModel(client, urlid) {
+        client.init(urlid, {
+            success: success,
+            error: error,
+            internal: 1,
+            ui_infos: 0,
+            ui_controls: 1,
+            ui_vr: 0,
+            ui_help: 0,
+            ui_setttings: 0,
+            ui_stop: 0,
+            watermark: 0,
+            fullscreen: 1
+        });
+    }
+
+    // Listen for click events on the buttons
+    startButton.addEventListener('click', function() {
+        loadModel(client, model);
+    });
