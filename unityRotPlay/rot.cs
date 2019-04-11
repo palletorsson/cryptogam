cc8f74117e1721106ce72eca941f7451e5936ded
plix <palle.torsson@gmail.com>
2019-04-11 08:05:35 +0200

    model sequence and object based

--- unityRotPlay/rot.cs
+++ unityRotPlay/rot.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class rot : MonoBehaviour
 {
     [Range(1f, 100f)]
-    public float speedUpDown = 10;
+    public float speedUpDown = 0;
     [Range(1f, 20f)]
     public float distanceUpDown = 1;
-    [Range(-10f, 10f)]
+    [Range(-50f, 50f)]
     public float offset = 0;
-    void Update()
+    void FixUpdate()
     {
         Vector3 mov = new Vector3(
               (Mathf.Cos(speedUpDown * Time.time) * distanceUpDown) + offset,
               transform.position.y,
               (Mathf.Sin(speedUpDown * Time.time) * distanceUpDown) + offset);
         transform.position = mov;
+        distanceUpDown = distanceUpDown + 0.01f;
+        if ( distanceUpDown > 10.0f) {
+
+            speedUpDown = 0;
+        }
+
     }
 }
