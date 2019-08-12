932631619410c8a49513c8e370d9dafc6f0467d8
plix <palle.torsson@gmail.com>
2019-08-12 21:31:45 +0200

    Update myScript.js

--- sketchfab/tutorial/step_4/js/myScript.js
+++ sketchfab/tutorial/step_4/js/myScript.js
@@ -1,6 +1,21 @@
 var iframe = document.getElementById( 'api-frame' );
 var uid = 'c966755a1efe451b80925b19ed6a9318';
 var client = new Sketchfab( iframe );
+var cameraPositions = [
+      {
+         "position": [0.17116218147772588,-0.5164971178787727,86.78621693231594],
+         "target": [-0.17600697144648275,-1.1942467879311174,74.6853367314544]
+      },
+      {
+          "position": [-0.4866313946699046,-1.8006774176324318,63.857545709273055],
+          "target": [-0.4866299583294329,-1.8006767819247784,63.85744572160981],
+      },
+      {
+          "position": [-6.2959436411001,2.5339732345834407,15.220666589705965],
+          "target": [-6.295946179228759,2.5339818999264883,15.220566998191604],
+      }];
+var posIndex = 0;
+
 client.init( uid, {
     success: function onSuccess( api ){
         api.start();
@@ -11,6 +26,8 @@ client.init( uid, {
                   // get the camera position by clicking on the far left of screen
                   if (info.position2D[0] < 100) {
                     promtCameraPosition(api);
+                  } else {
+                    setCamera(api);
                   }
                 }
             );
@@ -20,6 +37,12 @@ client.init( uid, {
         console.log( 'Viewer error' );
     }
 });
+
+function setCamera(api) {
+  api.setCameraLookAt(cameraPositions[posIndex].position, cameraPositions[posIndex].target, 3);
+  posIndex++; 
+}
+
 
 function promtCameraPosition(api) {
   api.getCameraLookAt(function(err, camera) {
